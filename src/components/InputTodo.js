3944aac645f9ab4776d708cbb49250e027768ef5
Abbas <abbasgulzaar@gmail.com>
2021-12-09 13:17:41 +0500

    Third part

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,7 +2,6 @@ import React, { Component } from "react"
 
 class InputTodo extends Component {
     state = {
-        // title: "",
         fName: "",
         lastName: "",
     };
@@ -24,14 +23,15 @@ class InputTodo extends Component {
     }
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit} className="form-container">
                 <input type="text"
+                    className="input-text"
                     placeholder="Add todo..."
                     value={this.state.title}
                     name="title"
                     onChange={this.onChange}
                 />
-                <button>Submit</button>
+                <button className="input-submit">Submit</button>
             </form>
         )
     }
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -61,14 +61,15 @@ class TodoContainer extends React.Component {
     };
     render() {
         return (
-            <div>
-                <Header />
-                {/* <InputTodo onChange={this.onChange} /> */}
-                <InputTodo addTodoProps={this.addTodoItem} />
-                <TodosList
-                    todos={this.state.todos}
-                    handleChangeProps={this.handleChange}
-                    deleteTodoProps={this.delTodo} />
+            <div className="container">
+                <div className="inner">
+                    <Header />
+                    <InputTodo addTodoProps={this.addTodoItem} />
+                    <TodosList
+                        todos={this.state.todos}
+                        handleChangeProps={this.handleChange}
+                        deleteTodoProps={this.delTodo} />
+                </div>
             </div>
         );
     }
