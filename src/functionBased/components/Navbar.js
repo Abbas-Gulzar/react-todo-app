74b91fee157d4ca173309fb7704390f1256629e5
Abbas <abbasgulzaar@gmail.com>
2021-12-14 13:07:47 +0500

    Tenth part

---
+++ src/functionBased/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { NavLink } from "react-router-dom"
+import { useState } from "react"
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false)
+    const links = [
+        {
+            id: 1,
+            path: "/",
+            text: "Home",
+        },
+        {
+            id: 2,
+            path: "/About",
+            text: "About",
+        },
+        {
+            id: 3,
+            path: "NotMatch",
+            text: "NotMatch",
+        },
+    ]
+    const handleToggle = () => {
+        setNavbarOpen(!navbarOpen)
+    }
+    const closeMenu = () => {
+        setNavbarOpen(false)
+    }
+    return (
+        <nav className="navBar">
+            <button onClick={handleToggle}>
+                {navbarOpen ? (
+                    <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+                ) : (
+                    <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+                )}
+            </button>
+            <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+                {links.map(link => {
+                    return (
+                        <li key={link.id}>
+                            <NavLink
+                                to={link.path}
+                                activeClassName="active-link"
+                                onClick={() => closeMenu()}
+                                exact
+                            >
+                                {link.text}
+                            </NavLink>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+    )
+}
+export default Navbar
--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -3,11 +3,12 @@ import Header from "./Header"
 import InputTodo from "./InputTodo"
 import TodosList from "./TodosList"
 import { v4 as uuidv4 } from "uuid"
-
-
+import { Route, Switch } from "react-router-dom"
+import About from "../pages/About"
+import NotMatch from "../pages/NotMatch"
+import Navbar from "./Navbar"
 const TodoContainer = () => {
   const [todos, setTodos] = useState(getInitialTodos())
-
   const handleChange = id => {
     setTodos(prevState =>
       prevState.map(todo => {
@@ -64,18 +65,29 @@ const TodoContainer = () => {
   }, [todos])
 
   return (
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={addTodoItem} />
-        <TodosList
-          todos={todos}
-          handleChangeProps={handleChange}
-          deleteTodoProps={delTodo}
-          setUpdate={setUpdate}
-        />
-      </div>
-    </div>
+    <switch>
+      <Navbar />
+      <Route exact path="/">
+        <div className="container">
+          <div className="inner">
+            <Header />
+            <InputTodo addTodoProps={addTodoItem} />
+            <TodosList
+              todos={todos}
+              handleChangeProps={handleChange}
+              deleteTodoProps={delTodo}
+              setUpdate={setUpdate}
+            />
+          </div>
+        </div>
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route exact path="/NotMatch">
+        <NotMatch />
+      </Route>
+    </switch>
   )
 }
 
